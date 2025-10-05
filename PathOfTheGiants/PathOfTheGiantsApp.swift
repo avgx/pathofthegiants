fffe84bb122fcd0faeedb2e2ad076a680eb6841c
avgx <agovorovsky@gmail.com>
2025-10-05 22:18:07 +0300

    create project

---
+++ PathOfTheGiants/PathOfTheGiantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathOfTheGiantsApp.swift
+//  PathOfTheGiants
+//
+//  Created by Alexey Govorovsky on 05.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct PathOfTheGiantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
