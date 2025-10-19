e0544802c7fbdb90454e44f9b8cdeb17d387c547
avgx <agovorovsky@gmail.com>
2025-10-20 01:18:43 +0300

    fixes #12 links, add accent color = orange

--- Packages/UI/Sources/UI/Screens/Profile/Settings/Profile+InfoView.swift
+++ Packages/UI/Sources/UI/Screens/Profile/Settings/Profile+InfoView.swift
@@ -7,6 +7,24 @@ extension Profile {
             List {
                 Section {
                     LabeledContent("Версия приложения", value: Bundle.main.versionBuild)
+                } footer: {
+                    HStack(spacing: 16) {
+                        Spacer()
+                        Link(destination: URL(string: "https://vk.com/pathofthegiants")!, label: {
+                            Label("@pathofthegiants", image: "vk")
+                        })
+                        Spacer()
+                        Link(destination: URL(string: "https://t.me/pathofthegiants")!, label: {
+                            Label("t.me/pathofthegiants", image: "tg")
+                        })
+                        Spacer()
+                        Link(destination: URL(string: "mailto:pathofthegiants@gmail.com")!, label: {
+                            Label("pathofthegiants@gmail.com", image: "email")
+                        })
+                        Spacer()
+                    }
+                    .labelStyle(.iconOnly)
+                    .padding()
                 }
             }
         }
